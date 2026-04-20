75fb6871f9469f62797505b107e14929f525cdfd
ap461 <ap@apscore5.com>
2026-04-20 10:24:43 -0500

    fix: NewsletterSignup

--- slices/NewsletterSignup/index.tsx
+++ slices/NewsletterSignup/index.tsx
@@ -1,32 +1,28 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
 
-/**
- * Props for `NewsletterSignup`.
- */
-export type NewsletterSignupProps =
-  SliceComponentProps<Content.NewsletterSignupSlice>;
+export type NewsletterSignupProps = SliceComponentProps<Content.NewsletterSignupSlice>;
 
-/**
- * Component for "NewsletterSignup" Slices.
- */
 const NewsletterSignup: FC<NewsletterSignupProps> = ({ slice }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for newsletter_signup (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+      <div className="container">
+        <div className="nl-box">
+          <div>
+            <PrismicRichText field={slice.primary.heading} />
+            <PrismicRichText field={slice.primary.description} />
+          </div>
+          <div>
+            <div className="nl-form">
+              <input placeholder={slice.primary.input_placeholder || "Your email address"} type="email" />
+              <button>{slice.primary.button_text || "Join Free"}</button>
+            </div>
+            <div className="nl-note">Free forever. Unsubscribe anytime.</div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
-
 export default NewsletterSignup;
